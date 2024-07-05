dbec0674f93a0ef4b4c1e8327bb1348346e9183b
Shiva833215 <112087623+Shiva833215@users.noreply.github.com>
2024-07-05 18:55:38 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,10 +7,10 @@ CPU_THRESHOLD=1
 MEMORY_USAGE=$(free | awk 'NR==2{printf "%.2f", $3/$2*100}')
 CPU_USAGE=$(mpstat | awk '$12 ~ /[0-9.]+/ {printf "%.2f", 100-$12}')
 
-if (( $(echo "$MEMORY_USAGE >= $MEMORY_THRESHOLD" | bc -l) )); then
-    echo "Memory usage is high: $MEMORY_USAGE%" 
+if (( $(echo "$CPU_USAGE > $CPU_THRESHOLD" | bc -l) )) || (( $(echo "$MEMORY_USAGE > $MEMORY_THRESHOLD" | bc -l) )); then
+    echo "Threshold exceeded, sending email notification."
+    echo -e "Resource Utilization Alert:\n\nCPU Utilization: ${CPU_USAGE}%\nMemory Utilization: ${MEMORY_USAGE}%\n\n"
+else
+    echo "Resource utilization is within acceptable limits."
 fi
-
-if (( $(echo "$CPU_USAGE >= $CPU_THRESHOLD" | bc -l) )); then
-    echo "CPU usage is high: $CPU_USAGE%"
-fi
+ 
